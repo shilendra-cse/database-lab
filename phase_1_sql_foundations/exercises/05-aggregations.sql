7d4afa5409117d001ab0020ea28f47e5d604c7d4
Shilendra Singh <shelendrasingh704@gmail.com>
2026-06-16 09:45:22 +0530

    phase 1 aggregate functions done

--- phase_1_sql_foundations/exercises/05-aggregations.sql
+++ phase_1_sql_foundations/exercises/05-aggregations.sql
@@ -1,9 +1,14 @@
 -- Total money spent
+SELECT SUM(price) as total_spent FROM expenses;
 
 -- Average expense
+SELECT AVG(price) as avg_expense_price FROM expenses;
 
 -- Highest expense
+SELECT MAX(price) FROM expenses;
 
 -- Lowest expense
+SELECT MIN(price) FROM expenses;
 
 -- Number of expenses
+SELECT COUNT(id) FROM expenses;
