2e20244dfb9ea091c88aa6127e01f6c4a5aafd0e
Shilendra Singh <shelendrasingh704@gmail.com>
2026-06-16 19:28:35 +0530

    phase 1 group by

--- phase_1_sql_foundations/exercises/06-group-by.sql
+++ phase_1_sql_foundations/exercises/06-group-by.sql
@@ -1,7 +1,15 @@
 -- Total spending per category
+SELECT SUM(price) FROM expenses
+GROUP BY category_id;
 
 -- Number of expenses per category
+SELECT COUNT(id) FROM expenses
+GROUP BY category_id;
 
 -- Average expense per category
+SELECT AVG(price) FROM expenses
+GROUP BY category_id;
 
 -- Highest expense per category
+SELECT MAX(price) FROM expenses
+GROUP BY category_id;
