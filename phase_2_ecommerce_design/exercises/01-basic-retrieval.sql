f490780a991ba489b81729f78ee983ffcc7bcd40
Shilendra Singh <shelendrasingh704@gmail.com>
2026-06-29 08:33:17 +0530

    phase 2 ecommerce seed

---
+++ phase_2_ecommerce_design/exercises/01-basic-retrieval.sql
@@ -0,0 +1,11 @@
+-- Get all users.
+select * from users;
+
+-- Get all products.
+select * from products;
+
+-- Get all orders.
+select * from orders;
+
+-- Get all order order items.
+select * from order_items;
